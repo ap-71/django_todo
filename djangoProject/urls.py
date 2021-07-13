df31da51eadfec48b3cefc42d2d4d9114f6c7ddc
Aleksandr <panarin0290@gmail.com>
2021-07-13 17:54:42 +0300

    Добавление маршрута: path('api/', include(router.urls)),

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -27,6 +27,7 @@ router.register('todo_del', todo.views.TaskDelAPI, basename='todo_del_api')
 
 urlpatterns = [
     path('', index, name='main'),
+    path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('todo/', todo.views.task, name='todo'),
